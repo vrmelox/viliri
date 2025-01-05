0136a023775d5493ab0b1cc2c4a7c5ea4651f5ad
vrmelox <akandeabiodoun@gmail.com>
2025-01-06 00:46:06 +0100

    I've created the first component : Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Navbar from "./components/Navbar"
+import logo from './assets/logo/logo.png'
+import './styles/app.css'
 
+const viliriMenu = {
+    logo: logo,
+};
 
 function App() {
-
+    return (
+        <div>
+            <Navbar menu={viliriMenu} />
+        </div>
+    );
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import '../styles/navbar.css'
+
+
+function Navbar({menu}) {
+    return (
+        <nav className='index-nav'>
+            <FontAwesomeIcon icon={faBars} className='icon'/>
+            <img
+                src={menu.logo}
+                alt="Le logo de la librairie Viliri"
+                className='logo_viliri'
+            />
+            <div className='hero-right'>
+                <p>
+                    <FontAwesomeIcon icon={faHeart} className='icon' />
+                    Whishlist
+                </p>
+                <p>
+                    <FontAwesomeIcon icon={faUser} className='icon' />
+                    Sign in
+                </p>
+            </div>
+        </nav>
+    )
+}
+
+Navbar.PropTypes = {
+    menu: PropTypes.shape({
+        logo: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default Navbar;
