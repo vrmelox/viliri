33603424848fd9844e187a020c4df9cd3ccc8bea
vrmelox <akandeabiodoun@gmail.com>
2025-01-08 08:48:31 +0100

    I've added Swiper library for the sliding of popular books section

--- src/components/Bookpopular.jsx
+++ src/components/Bookpopular.jsx
@@ -1,29 +1,56 @@
 import PropTypes from 'prop-types';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight, faStar } from '@fortawesome/free-solid-svg-icons';
 import '../styles/bookpopular.css'
 
 function Bookpopular ({books}) {
     return (
-        <section className="livres-populaires">
-            <FontAwesomeIcon icon={faChevronLeft} className='icon-left'/>
-            <div className="books-list">
-                {books.map((book, index) => (
-                    <div className="book" key={index}>
-                        <img src={book.image} alt={book.title} />
-                        <h3>{book.title}</h3>
-                        <h4>{book.auteur}</h4>
-                        <div className='notation'>
-                            {[...Array(5)].map((_, i) => (
-                                <FontAwesomeIcon key={i} icon={faStar} />
-                            ))}
+    <section className="livres-populaires">
+        <FontAwesomeIcon icon={faChevronLeft} className='icon-left'/>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={5}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                    >
+                    {books.map((book, index) => (
+                        <div className="book" key={index}>
+                            <SwiperSlide key={index}>
+                            <img src={book.image} alt={book.title} />
+                            <h3>{book.title}</h3>
+                            <h4>{book.auteur}</h4>
+                            <div className='notation'>
+                                {[...Array(5)].map((_, i) => (
+                                    <FontAwesomeIcon key={i} icon={faStar} />
+                                ))}
+                            </div>
+                            </SwiperSlide>
                         </div>
-                    </div>
                     ))}
-            </div>
-            <FontAwesomeIcon icon={faChevronRight} className='icon-right'/>
-        </section>
-    )
+        </Swiper>
+        <FontAwesomeIcon icon={faChevronRight} className='icon-right'/>
+    </section>
+        // <section className="livres-populaires">
+        //     <FontAwesomeIcon icon={faChevronLeft} className='icon-left'/>
+        //     <div className="books-list">
+        //         {books.map((book, index) => (
+        //             <div className="book" key={index}>
+        //                 <img src={book.image} alt={book.title} />
+        //                 <h3>{book.title}</h3>
+        //                 <h4>{book.auteur}</h4>
+        //                 <div className='notation'>
+        //                     {[...Array(5)].map((_, i) => (
+        //                         <FontAwesomeIcon key={i} icon={faStar} />
+        //                     ))}
+        //                 </div>
+        //             </div>
+        //             ))}
+        //     </div>
+        //     <FontAwesomeIcon icon={faChevronRight} className='icon-right'/>
+        // </section>
+    );
 }
 
 Bookpopular.PropTypes = {
--- src/data/libraryDatas.js
+++ src/data/libraryDatas.js
@@ -8,27 +8,27 @@ export const libraryDatas = [
         genre: 'Fantasy',
         auteur: 'G. G. Martin',
         title: 'Feu et sang',
-        image: '../public/feu_et_sang.jpg',
+        image: '/feu_et_sang.jpg',
         status: true
     },
     {
         genre: 'Fantasy',
         auteur: 'G. G. Martin',
         title: 'Le chevalier errant',
-        image: '../public/le_chevalier_errant.jpg',
+        image: '/le_chevalier_errant.jpg',
         status: true
     },
     {
         genre: 'Fantasy',
         auteur: 'G. G. Martin',
         title: 'Le dragon de glace',
-        image: '../public/dragon_de_glace.jpg'
+        image: '/dragon_de_glace.jpg'
     },
     {
         genre: 'Politique',
         auteur: 'Jean Pliya',
         title: 'Les tresseurs de corde',
-        image: '../public/tresseurs_cord.jpg'
+        image: '/tresseurs_cord.jpg'
     },
     {
         genre: 'Horreur',
@@ -39,44 +39,44 @@ export const libraryDatas = [
         genre: 'Romance',
         auteur: 'Jean Pliya',
         title: 'La secrétaire particulière',
-        image: '../public/la_secretaire_particuliere.jpg'
+        image: '/la_secretaire_particuliere.jpg'
     },
     {
         genre: 'Jeunesse',
         auteur: 'J. K. Rowling',
         title: 'Harry Potter à l\'école des sorciers',
-        image: '../public/ecole_des_sorciers.jpg',
+        image: '/ecole_des_sorciers.jpg',
         status: true
     },
     {
         genre: 'Jeunesse',
         auteur: 'J. K. Rowling',
         title: 'Harry Potter et la coupe de feu',
-        image: '../public/la_coupe_de_feu.jpg'
+        image: '/la_coupe_de_feu.jpg'
     },
     {
         genre: 'Jeunesse',
         auteur: 'J. K. Rowling',
         title: 'Harry Potter et les reliques de la mort',
-        image: '../public/reliques_mort.jpg'
+        image: '/reliques_mort.jpg'
     },
     {
         genre: 'Jeunesse',
         auteur: 'J. K. Rowling',
         title: 'Harry Potter et le prince de sang mêlé',
-        image: '../public/prince_sang_mele.jpg'
+        image: '/prince_sang_mele.jpg'
     },
     {
         genre: 'Horreur',
         auteur: 'Olympe Bhêly-Quenum',
         title: 'Un piège sans fin',
-        image: '../public/piege_sans_fin.jpg'
+        image: '/piege_sans_fin.jpg'
     },
     {
         genre: 'Science-fiction',
         auteur: 'Gaston Zossou',
         title: 'La guerre des choses dans l\'ombre',
-        image: '../public/la_guerre_des_choses_dans_lombre.jpg'
+        image: '/la_guerre_des_choses_dans_lombre.jpg'
     },
     {
         genre: 'Fantasy',
@@ -87,7 +87,7 @@ export const libraryDatas = [
         genre: 'Aventure',
         auteur: 'Florent Couao-Zotti',
         title: 'Notre pain de chaque nuit',
-        image: '../public/pain_chaque_nuit.jpg'
+        image: '/pain_chaque_nuit.jpg'
     },
     {
         genre: 'Policier',
@@ -103,7 +103,7 @@ export const libraryDatas = [
         genre: 'Traité',
         auteur: 'Nicols Boileau',
         title: 'L\'Art poétique',
-        image: '../public/art_poetique.jpg',
+        image: '/art_poetique.jpg',
         status: true
 
     },
@@ -111,7 +111,7 @@ export const libraryDatas = [
         genre: 'Satirique',
         auteur: 'Nicolas Boileau',
         title: 'Les satires',
-        image: '../public/satires.jpg'
+        image: '/satires.jpg'
     },
     {
         genre: 'Policier',
@@ -132,7 +132,7 @@ export const libraryDatas = [
         genre: 'Policier',
         auteur: 'Edgar Allan Poe',
         title: 'Double assassinat dans la rue',
-        image: '../public/double_assassinat.jpg',
+        image: '/double_assassinat.jpg',
         status: true
     },
     {
@@ -144,7 +144,7 @@ export const libraryDatas = [
         genre: 'Romance',
         auteur: 'Edgar Allan Poe',
         title: 'Le scarabée d\'or',
-        image: '../public/scarabee_or.jpg'
+        image: '/scarabee_or.jpg'
     },
     {
         genre: 'Policier',
@@ -165,13 +165,13 @@ export const libraryDatas = [
         genre: 'Fantasy',
         auteur: 'J. R. R. Tolkien',
         title: 'Le silmarillion',
-        image: '../public/silmarillion.jpg'
+        image: '/silmarillion.jpg'
     },
     {
         genre: 'Fantasy',
         auteur: 'J. R. R. Tolkien',
         title: 'Beren of Luthien',
-        image: '../public/beren_luthen.jpg'
+        image: '/beren_luthen.jpg'
     },
     {
         genre: 'Fantasy',
@@ -197,13 +197,13 @@ export const libraryDatas = [
         genre: 'Fantasy',
         auteur: 'Andrzej Sapkowski',
         title: 'La dame du lac',
-        image: '../public/le_chant_du_lac.jpg'
+        image: '/le_chant_du_lac.jpg'
     },
     {
         genre: 'Fantasy',
         auteur: 'Andrzej Sapkowski',
         title: 'The Witcher',
-        image: '../public/witcher.jpg',
+        image: '/witcher.jpg',
         status: true
     },
     {
@@ -215,7 +215,7 @@ export const libraryDatas = [
         genre: 'Business',
         auteur: 'Robert Kiyosaki',
         title: 'Le cadran du cashflow',
-        image: '../public/le_cadran_du_cashflow.jpg',
+        image: '/le_cadran_du_cashflow.jpg',
         status: true
     },
 ]
